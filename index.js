65b054eccfc21e7865be3d34e6a491036221b353
David Gomes <98854034+DavidGomesFarias@users.noreply.github.com>
2024-05-02 22:55:22 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,134 @@
+const h1 = document.querySelector('.h-1')
+const h2 = document.querySelector('.h-2')
+const h3 = document.querySelector('.h-3')
+const h4 = document.querySelector('.h-4')
+const h5 = document.querySelector('.h-5')
+const h6 = document.querySelector('.h-6')
+const h7 = document.querySelector('.h-7')
+const h8 = document.querySelector('.h-8')
+const h9 = document.querySelector('.h-9')
+const h10 = document.querySelector('.h-10')
+const svg1 = document.querySelectorAll('.svg-1')
+const svg2 = document.querySelectorAll('.svg-2')
+const divNone = document.querySelectorAll('.div-none')
+
+
+h1.addEventListener('click', () => {
+    if(divNone[0].style.display == 'none') {
+        divNone[0].style.display = 'block'
+        svg1[0].style.display = 'none'
+        svg2[0].style.display = 'block'
+    } else {
+        divNone[0].style.display = 'none'
+        svg1[0].style.display = 'block'
+        svg2[0].style.display = 'none'
+    }
+})
+
+h2.addEventListener('click', () => {
+    if(divNone[1].style.display == 'none') {
+        divNone[1].style.display = 'block'
+        svg1[1].style.display = 'none'
+        svg2[1].style.display = 'block'
+    } else {
+        divNone[1].style.display = 'none'
+        svg1[1].style.display = 'block'
+        svg2[1].style.display = 'none'
+    }
+})
+
+h3.addEventListener('click', () => {
+    if(divNone[2].style.display == 'none') {
+        divNone[2].style.display = 'block'
+        svg1[2].style.display = 'none'
+        svg2[2].style.display = 'block'
+    } else {
+        divNone[2].style.display = 'none'
+        svg1[2].style.display = 'block'
+        svg2[2].style.display = 'none'
+    }
+})
+
+h4.addEventListener('click', () => {
+    if(divNone[3].style.display == 'none') {
+        divNone[3].style.display = 'block'
+        svg1[3].style.display = 'none'
+        svg2[3].style.display = 'block'
+    } else {
+        divNone[3].style.display = 'none'
+        svg1[3].style.display = 'block'
+        svg2[3].style.display = 'none'
+    }
+})
+
+h5.addEventListener('click', () => {
+    if(divNone[4].style.display == 'none') {
+        divNone[4].style.display = 'block'
+        svg1[4].style.display = 'none'
+        svg2[4].style.display = 'block'
+    } else {
+        divNone[4].style.display = 'none'
+        svg1[4].style.display = 'block'
+        svg2[4].style.display = 'none'
+    }
+})
+
+h6.addEventListener('click', () => {
+    if(divNone[5].style.display == 'none') {
+        divNone[5].style.display = 'block'
+        svg1[5].style.display = 'none'
+        svg2[5].style.display = 'block'
+    } else {
+        divNone[5].style.display = 'none'
+        svg1[5].style.display = 'block'
+        svg2[5].style.display = 'none'
+    }
+})
+
+h7.addEventListener('click', () => {
+    if(divNone[6].style.display == 'none') {
+        divNone[6].style.display = 'block'
+        svg1[6].style.display = 'none'
+        svg2[6].style.display = 'block'
+    } else {
+        divNone[6].style.display = 'none'
+        svg1[6].style.display = 'block'
+        svg2[6].style.display = 'none'
+    }
+})
+
+h8.addEventListener('click', () => {
+    if(divNone[7].style.display == 'none') {
+        divNone[7].style.display = 'block'
+        svg1[7].style.display = 'none'
+        svg2[7].style.display = 'block'
+    } else {
+        divNone[7].style.display = 'none'
+        svg1[7].style.display = 'block'
+        svg2[7].style.display = 'none'
+    }
+})
+
+h9.addEventListener('click', () => {
+    if(divNone[8].style.display == 'none') {
+        divNone[8].style.display = 'block'
+        svg1[8].style.display = 'none'
+        svg2[8].style.display = 'block'
+    } else {
+        divNone[8].style.display = 'none'
+        svg1[8].style.display = 'block'
+        svg2[8].style.display = 'none'
+    }
+})
+
+h10.addEventListener('click', () => {
+    if(divNone[9].style.display == 'none') {
+        divNone[9].style.display = 'block'
+        svg1[9].style.display = 'none'
+        svg2[9].style.display = 'block'
+    } else {
+        divNone[9].style.display = 'none'
+        svg1[9].style.display = 'block'
+        svg2[9].style.display = 'none'
+    }
+})
